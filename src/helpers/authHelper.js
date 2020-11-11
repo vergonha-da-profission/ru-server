4f1c686c56b3d107fc59ea9a264c063c9b20def6
Mayara.Ogliari <prog31@ixcsoft.com.br>
2020-11-10 22:45:41 -0300

    correção de código de acordo com linter

--- src/helpers/authHelper.js
+++ src/helpers/authHelper.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET;
 
-exports.generateToken = tokenData => {
-    return jwt.sign(tokenData, JWT_SECRET, { expiresIn: '1h' });
-}
+const { JWT_SECRET } = process.env;
+
+exports.generateToken = (tokenData) => jwt.sign(tokenData, JWT_SECRET, { expiresIn: '1h' });

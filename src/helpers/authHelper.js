393b9365ec2d292e01f34658d69dd857fe2acd6c
Mayara.Ogliari <prog31@ixcsoft.com.br>
2020-11-10 22:02:41 -0300

    criação token - alteração do mesmo que estava gerando token incorreto

--- src/helpers/authHelper.js
+++ src/helpers/authHelper.js
@@ -2,8 +2,5 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 exports.generateToken = tokenData => {
-    return jwt.sign({
-        userId: tokenData.userId,
-        nickname: tokenData.nickname
-    }, JWT_SECRET, { expiresIn: '1h' });
+    return jwt.sign(tokenData, JWT_SECRET, { expiresIn: '1h' });
 }
